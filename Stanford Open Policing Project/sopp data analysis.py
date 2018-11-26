a4581989532b3e1fa9903543a2f7fa25bd249027
JalalDlimi <24705153+JalalDlimi@users.noreply.github.com>
2018-11-26 20:40:34 +0000

    Update sopp data analysis.py

--- Stanford Open Policing Project/sopp data analysis.py
+++ Stanford Open Policing Project/sopp data analysis.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In this project, I performed a data analysis on the Stanford Open Policing Project for the Rhode Island data using pandas.
-# I explained every step and what results mean during my work! Enjoy learning !
-# 
+
 # You can find the dataset I used in this repo.
 # 
 # * `sopp.csv` is the Rhode Island dataset from the [Stanford Open Policing Project](https://openpolicing.stanford.edu/), made available under the [Open Data Commons Attribution License](https://opendatacommons.org/licenses/by/summary/)
@@ -18,8 +16,6 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 
 
 # ## Stanford Open Policing Project Dataset
-# 
-# To understand what each column of the stanford dataset means, check this where you can find each column meaning so that you can be able to well understand the dataset and perform good data analysis! (check thhe stanford open policing website for it Jey)*
 
 # In[2]:
 
@@ -36,101 +32,109 @@ df.head()
 
 # In our dataset, each row represents a one traffic stop !
 
-# In[4]:
+# In[3]:
 
 
 df.shape
 
 
-# In[5]:
+# In[4]:
 
 
 df.dtypes
 
 
-# In[6]:
+# In[5]:
 
 
 df.isnull().sum()
 
 
-# In[7]:
+# In[6]:
 
 
 # As you can notice, we have a whole column with missing values ("county_name") - we will delete it!
 # I used inplace=True to avoid re-assigning the dataframe
 df.drop('county_name', axis=1, inplace=True)
 
+# alternative method :
+# ri.dropna(axis='columns', how='all').shape
 
 
-# In[8]:
+# In[7]:
 
 
 df.shape # we got now 14 columns
 
 
-# In[9]:
+# In[8]:
 
 
 df.columns  # the columns that r left
 
 
-# In[10]:
+# In[9]:
 
 
 df.loc[df.violation == 'Speeding', 'driver_gender'].value_counts()
 # checking how many men and women get pulled over because of speedingg
 
 
-# In[11]:
+# In[10]:
 
 
 # checking for example how many 50yo drivers get pulled over for different violations
 df[df.driver_age==50].violation.value_counts()
 
 
-# In[12]:
+# In[11]:
 
 
 # how often men and women violate nd if search is conducted or not.
 df.groupby(['search_conducted','driver_gender']).violation.value_counts().unstack()
 
 
-# In[13]:
+# In[12]:
 
 
 # checking how much search operations were conducted
 df.search_conducted.value_counts(normalize=True) # which is also the mean of how much the search was actually conducted
 
 
-# In[14]:
+# In[13]:
 
 
 # search rate by gender
 df.groupby('driver_gender').search_conducted.mean()
 
 
-# In[16]:
+# In[14]:
 
 
 # including the violation type as a second factor
 df.groupby(['violation', 'driver_gender']).search_conducted.mean()
 
 
-# In[17]:
+# In[15]:
 
 
 df.isnull().sum()
 
 
-# In[23]:
+# In[103]:
 
 
 # we noticed that search_type got +88k mmissig values -- WHY ? It's because if no search is conducted, there is no search type !
 df.search_type.value_counts()
 
 
-# In[20]:
+# In[104]:
+
+
+df.search_type.value_counts().plot(kind="bar")
+
+
+# In[17]:
 
 
 df[df.search_conducted == False].search_type.value_counts(dropna=False)
@@ -142,39 +146,39 @@ df[df.search_conducted == False].search_type.value_counts(dropna=False)
 # ##### we will create a new column with name "Frist" where will be assigning all the search types that contains "Protective Frisk"
 # 
 
-# In[35]:
+# In[18]:
 
 
 # includes partial matches
 df['Frisk'] = df.search_type.str.contains('Protective Frisk')
 
 
-# In[36]:
+# In[19]:
 
 
 df.columns
 
 
-# In[37]:
+# In[20]:
 
 
 df.Frisk.dtype
 
 
-# In[38]:
+# In[21]:
 
 
 # includes exact matches only
 df.Frisk.value_counts()
 
 
-# In[39]:
+# In[96]:
 
 
 df.Frisk.value_counts(dropna=False)
 
 
-# In[41]:
+# In[23]:
 
 
 df.Frisk.mean()
@@ -186,27 +190,87 @@ df.Frisk.mean()
 # ##### we will create now a column with the name "Suspicion" where we will be assigning all the search types that contains "Reasonable Suspicion"
 # 
 
-# In[42]:
+# In[24]:
 
 
 df['Suspicion']= df.search_type.str.contains('Reasonable Suspicion')
 
 
-# In[43]:
+# In[25]:
 
 
 df.columns
 
 
-# In[46]:
+# In[26]:
 
 
 df.Suspicion.value_counts()
 
 
-# In[47]:
+# In[27]:
 
 
 df.Suspicion.mean()
 
+
+# ###### Let's now use the stop_date and stop_time to get more insights about the data!
+
+# In[97]:
+
+
+# Getting the year with least number of stops !
+df.stop_date.str.slice(0,4).value_counts().plot(kind='pie')
+
+
+# In[60]:
+
+
+# We create a new column to save stop dates and time , both combined
+
+combined = df.stop_date.str.cat(df.stop_time, sep = " ") # conct
+df['datatime']=pd.to_datetime(combined) # I change the combined string type to a pandas datetime for future use!
+
+
+# In[53]:
+
+
+df.dtypes
+
+
+# In[87]:
+
+
+df.datatime.dt.year.value_counts().sort_values() # another way of counting how many stops per years using pandas data&time !
+
+
+# In[59]:
+
+
+# Getting the year with least stops ! 
+df.datatime.dt.year.value_counts().sort_values().index[0]
+
+
+# <b> Drug activity changes in time !
+
+# In[102]:
+
+
+df.groupby(df.datatime.dt.hour).drugs_related_stop.mean()
+
+
+# In[113]:
+
+
+df.groupby(df.datatime.dt.hour).drugs_related_stop.mean().plot()
+
+
+# In[117]:
+
+
+# count drug-related stops by hour instead of using the mean
+df.groupby(df.datatime.dt.hour).drugs_related_stop.sum().plot()
+
+
+# We all know that most accidents happen at night, would be great to make sure about that using data !
 
